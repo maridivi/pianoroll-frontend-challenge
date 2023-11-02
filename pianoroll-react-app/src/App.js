ce4d9bb0d5635975b2d458ed612a1ad474cd78ad
maridivi <maridivi89@gmail.com>
2023-11-02 16:45:19 +0000

    Create initial structure for App component

---
+++ pianoroll-react-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import PianoRollDisplay from "./components/PianorollDisplay";
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <div className="logo-container">
+          <img src="/white.svg" alt="Logo" />
+        </div>
+      </nav>
+
+      <h1> Welcome to PianoRoll frontend coding challenge!</h1>
+
+      <PianoRollDisplay />
+    </div>
+  );
+}
+
+export default App;
