37841f1beb586eb9dd1b5085e1afee9ecda23339
maridivi <maridivi89@gmail.com>
2023-11-05 21:45:35 +0000

    Create context for selected piano roll

--- pianoroll-react-app/src/App.js
+++ pianoroll-react-app/src/App.js
@@ -1,21 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./styles.css";
 import PianoRollDisplay from "./components/PianorollDisplay";
+import { createContext, useState } from "react";
+
+export const AppContext = createContext({
+  selectedPianorollIndex: undefined,
+  setSelectedPianorollIndex: () => {},
+});
 
 function App() {
+  const [selectedPianorollIndex, setSelectedPianorollIndex] = useState();
+
   return (
-    <div className="App">
-      <nav className="navbar">
-        <div className="logo-container">
-          <img src="/white.svg" alt="Logo" />
-        </div>
-      </nav>
+    <AppContext.Provider
+      value={{ selectedPianorollIndex, setSelectedPianorollIndex }}
+    >
+      <div className="App">
+        <nav className="navbar">
+          <div className="logo-container">
+            <img
+              src="/white.svg"
+              alt="Logo"
+              onClick={() => setSelectedPianorollIndex(undefined)}
+            />
+          </div>
+        </nav>
 
-      {/* <h1> Welcome to PianoRoll frontend coding challenge!</h1> */}
-
-      <PianoRollDisplay />
-    </div>
+        <PianoRollDisplay />
+      </div>
+    </AppContext.Provider>
   );
 }
 
