9d2e11c48939a1e03c5e330acfcff71b8cba7596
maridivi <maridivi89@gmail.com>
2023-11-06 15:03:14 +0000

    Comment useSelectNotesRange

--- pianoroll-react-app/src/utils/useSelectNotesRange.js
+++ pianoroll-react-app/src/utils/useSelectNotesRange.js
@@ -1,24 +1,31 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function useSelectNotesRange(svgRef, options) {
+  // Destructure the isActive property from options, defaulting to an empty object if options is not provided
   const { isActive } = options ?? {};
 
+  // Create a ref to track if the user is currently selecting
   const isSelecting = useRef(false);
 
+  // Create a state for the current selection
   const [selection, setSelection] = useState();
 
+  // Create refs for the start of the current selection (in both relative and pixel coordinates)
   const currentSelectionStart = useRef(); // 0 to 1
   const currentSelectionStartX = useRef(); // px
 
   const onMouseDown = useCallback(
     (e) => {
+      // If the user is already selecting, do nothing
       if (isSelecting.current) return;
 
+      // Calculate the relative start of the selection
       const cursorX = e.clientX;
       const svgRect = svgRef.current.getBoundingClientRect();
       const start = (cursorX - svgRect.left) / svgRect.width;
-      setSelection();
 
+      // Reset the selection and update the start refs
+      setSelection();
       currentSelectionStart.current = start;
       currentSelectionStartX.current = start * svgRect.width;
 
@@ -31,9 +38,12 @@ export default function useSelectNotesRange(svgRef, options) {
     (e) => {
       if (!isSelecting.current) return;
 
+      // Calculate the relative end of the selection
       const cursorX = e.clientX;
       const svgRect = svgRef.current.getBoundingClientRect();
 
+      // If the cursor is to the right of the start, update the end of the selection
+      // Otherwise, update the start of the selection
       if (cursorX >= currentSelectionStartX.current) {
         const end = (cursorX - svgRect.left) / svgRect.width;
         setSelection(() => ({
@@ -51,17 +61,23 @@ export default function useSelectNotesRange(svgRef, options) {
     [isSelecting, svgRef]
   );
 
+  // Define a callback to calculate the number of notes in the current selection
   const getNotesNumber = useCallback(() => {
+    // If there is no SVG or selection, do nothing
     if (!svgRef.current || !selection) return;
 
+    // Get all note elements in the SVG
     const notesEls = Array.from(
       svgRef.current.querySelectorAll(".note-rectangle")
     );
+
+    // Filter the notes based on their x and width attributes
     const filteredEls = notesEls.filter((el) => {
       const x = parseFloat(el.getAttribute("x"));
       const width = parseFloat(el.getAttribute("width"));
       const end = x + width;
 
+      // If the note starts or ends within the selection, include it
       if (x >= selection.start && x <= selection.end) {
         return true;
       } else if (end >= selection.start && end <= selection.end) {
@@ -70,11 +86,14 @@ export default function useSelectNotesRange(svgRef, options) {
 
       return false;
     });
+
+    // Log the number of notes in the selection
     console.log(filteredEls.length);
   }, [selection, svgRef]);
 
   const onMouseUp = useCallback(
     (e) => {
+      // Log the selection and calculate the number of notes in it
       console.log(selection);
       getNotesNumber();
       isSelecting.current = false;
@@ -84,6 +103,7 @@ export default function useSelectNotesRange(svgRef, options) {
     [getNotesNumber, selection]
   );
 
+  // Add event listeners when isActive is true
   useEffect(() => {
     if (!isActive) return;
 
@@ -93,6 +113,7 @@ export default function useSelectNotesRange(svgRef, options) {
       svg.addEventListener("mousemove", onMouseMove);
       svg.addEventListener("mouseup", onMouseUp);
 
+      // Clean up the event listeners when the component unmounts or isActive changes
       return () => {
         svg.removeEventListener("mousedown", onMouseDown);
         svg.removeEventListener("mousemove", onMouseMove);
@@ -101,5 +122,6 @@ export default function useSelectNotesRange(svgRef, options) {
     }
   }, [svgRef, onMouseDown, onMouseMove, onMouseUp, isActive]);
 
+  // Return the current selection
   return { selection };
 }
